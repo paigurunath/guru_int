6f2b2d9bff3c71e705f40dc0c9f8eb5d4e11d102
Gurunath Pai <pai.gurunath@gmail.com>
2017-07-07 13:48:18 +0530

    changes to display all values at once

--- secure.js
+++ secure.js
@@ -1,5 +1,59 @@
 var noble = require('noble');
 
+
+// // const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+// var Sequelize = require('sequelize');
+
+// const sequelize = new Sequelize('db', 'root', 'root', {
+//   host: 'localhost',
+//   dialect: 'mysql'
+// });
+
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
+
+
+// //to create the table
+// const Sensor = sequelize.define('sensor', {
+//   accelx: {
+//     type: Sequelize.REAL
+//   },
+//   accely: {
+//     type: Sequelize.REAL
+//   },
+//   accelz: {
+//     type: Sequelize.REAL
+//   },
+//   gyrox: {
+//     type: Sequelize.REAL
+//   },
+//   gyroy: {
+//     type: Sequelize.REAL
+//   },
+//   gyroz: {
+//     type: Sequelize.REAL
+//   }
+// });
+
+// // force: true will drop the table if it already exists
+// Sensor.sync({force: true}).then(() => {
+//   // Table created
+//   return Sensor.create({
+//     accelx: 0,
+//     accely: -1,
+//     accelz: 0,
+//     gyrox: 0,
+//     gyroy: -1,
+//     gyroz: 0
+//   });
+// });
+
 // Search only for the Service UUID of the device (remove dashes)
 var serviceUuids = ['2947ac9efc3811e586aa5e5517507c66'];
 
@@ -10,6 +64,8 @@ var AZ_CHAR_UUID = '2947b180fc3811e586aa5e5517507c66';
 var GX_CHAR_UUID = '2947b252fc3811e586aa5e5517507c66';
 var GY_CHAR_UUID = '2947b5aefc3811e586aa5e5517507c66';
 var GZ_CHAR_UUID = '2947b694fc3811e586aa5e5517507c66';
+
+var i=1;
 
 var uid_array = ['2947af14fc3811e586aa5e5517507c66','2947b090fc3811e586aa5e5517507c66','2947b180fc3811e586aa5e5517507c66','2947b252fc3811e586aa5e5517507c66','2947b5aefc3811e586aa5e5517507c66','2947b694fc3811e586aa5e5517507c66']
 // start scanning when bluetooth is powered on
@@ -71,13 +127,43 @@ function getSocketLabel(uuid) {
   return label;
 }
 
+var ax = 0;
+var ay = 0;
+var az = 0;
+var gx = 0;
+var gy = 0;
+var gz = 0;
+
 function emitSensorData(characteristic) {
 console.log("call to emit")
   var socketLabel = getSocketLabel(characteristic.uuid);
   //console.log(socketLabel);
 
-  characteristic.on('read', function(data) {
-    console.log(socketLabel + "--" + data.readInt32LE(0));
-  });
+  if(i<7) {
+    characteristic.on('read', function(data) {
+      console.log(socketLabel + "--" + data.readInt32LE(0));
+
+      if(i == 1) {
+        ax = data.readInt32LE(0);
+      } else if(i == 2) {
+        ay = data.readInt32LE(0);
+      } else if(i == 3) {
+        az = data.readInt32LE(0);
+      } else if(i == 4) {
+        gx = data.readInt32LE(0);
+      } else if(i == 5) {
+        gy = data.readInt32LE(0);
+      } else if(i == 6) {
+        gz = data.readInt32LE(0);
+
+        console.log(ax + " : " + ay + " : " + az + " : " + gx + " : " + gy + " : " + gz);
+        i == 0;
+      } 
+    });
+    i++;
+
+    
+  } 
+  console.log("My i : " + i);
   characteristic.notify('true', function(error) { if (error) throw error; });
 }
